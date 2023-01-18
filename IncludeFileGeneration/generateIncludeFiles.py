07bd2d4483351d3d0c05aed6bd66cf1879955c15
arunsug <arunsug@gmail.com>
2023-01-18 00:34:49 -0800

    first commit

---
+++ IncludeFileGeneration/generateIncludeFiles.py
@@ -0,0 +1,12 @@
+table = [["q","w","e","r", "t"],["a","s","d","f"],]
+
+
+for row in table:
+    inputName = 
+    outputName = 
+    length = 
+    for block in row:
+        fileName = intputName + "block"
+        includeFile = open(fileName, "w+")
+        inputLine = "input " +  + fileName
+        includeFile.write("")
