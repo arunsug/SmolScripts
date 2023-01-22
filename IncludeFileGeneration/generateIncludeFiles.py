dd2104a9736d64f544a6424f8d758b5a08fe21ef
Arun Sugumar <arunsug@gmail.com>
2023-01-22 15:48:23 -0800

    minor fixes

--- IncludeFileGeneration/generateIncludeFiles.py
+++ IncludeFileGeneration/generateIncludeFiles.py
@@ -3,16 +3,15 @@ import pandas as pd
 files = {}
 table = open("Yosemite.csv", "r")
 
+folder = "IncludeFiles/"
 inputNameIndex = 0
 lengthIndex = 1
 outputNameIndex = 10
-blocks = [5,6,7]
+blocks = [4,5,6]
 print(table.readline())
 
 for line in table:
-    print(line)
     row = line.split(",")
-    print(row)
     inputWire = row[inputNameIndex].strip().split()
     if inputWire == []:
         continue
@@ -28,7 +27,7 @@ for line in table:
         if module != "":
             fileName = module + "_InOut.vh"
             if fileName not in files:
-                files[fileName] = open(fileName, "w+")
+                files[fileName] = open(folder + fileName, "w+")
             inputLine = "input logic " + lengthString + inputName + ",\n"
             files[fileName].write(inputLine)
             if i == len(blocks)-1:
@@ -40,7 +39,7 @@ for line in table:
 
             fileName = module + "_Connect.vh"
             if fileName not in files:
-                files[fileName] = open(fileName, "w+")
+                files[fileName] = open(folder + fileName, "w+")
             files[fileName].write("assign " + outputName + " = " + inputName + ";\n")
             inputName = outputName
             i += 1
